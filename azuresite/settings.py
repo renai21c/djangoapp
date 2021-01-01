5566334099857765a27adc707fb791484037cd4f
YK Cho <renai21c@gmail.com>
2021-01-01 21:28:28 +0900

    Update settings.py
    DEBUG to False

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-^rq(x*d--6_#635*j84d5(fz9@-3(9vdr_s$9+^@cw08dq(ja'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
